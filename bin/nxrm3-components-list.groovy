273fefba74fa8c7045de2cc12a4340e5a8321ad7
sotudeko <sotudeko@sonatype.com>
2019-04-21 18:26:13 +0200

    0.5

---
+++ bin/nxrm3-components-list.groovy
@@ -0,0 +1,103 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput;
+
+class NXRM3Components {
+
+   static void main(String[] args) {
+
+      def dockerRepoConnector    
+      def repositoryUrl = args[0]
+      def repositoryName = args[1]
+
+      if (args.length > 2){
+         dockerRepoConnector = args[2]
+      }
+
+      def endpoint = repositoryUrl + '/service/rest/v1/components'
+      def query = '?repository=' + repositoryName
+      def repoAddress = endpoint + query
+
+      def continueToken = 'start'
+      int numberOfComponents = 0
+
+      while (continueToken != null){
+
+         def url = new URL(repoAddress)
+         def connection = url.openConnection()
+         connection.requestMethod = 'GET'
+
+         if (connection.responseCode == 200) {
+            def content = connection.content.text
+
+            def jsonSlurper = new JsonSlurper()
+            def jsonObject = jsonSlurper.parseText(content)
+
+            jsonObject.items.each { 
+
+               switch(it.format){
+                  case 'maven2': printList(it); break
+                  case 'npm': printNpm(it); break
+                  case 'nuget': printList(it); break
+                  case 'docker': printDocker(it, dockerRepoConnector); break
+                  default: break
+               }
+
+               numberOfComponents++
+            }
+
+            continueToken = jsonObject.continuationToken
+
+            if (continueToken != null){
+               repoAddress = endpoint + query + '&continuationToken=' + continueToken
+            } 
+         }
+      }
+
+      // println 'Number of components: ' + numberOfComponents
+   }
+
+   static def printRaw(it){
+      def pretty = JsonOutput.toJson(it)
+      println pretty
+   }
+
+   static printList(it){
+
+      def listing
+
+      if (it.format == 'maven2'){
+         listing = it.group + '.' + it.name + ':' + it.version // + ' (' + it.id + ')'
+      }
+      else {
+         listing = it.name + ':' + it.version // + ' (' + it.id + ')'
+      }
+
+      println listing
+      println ' - Assets'
+      for (asset in it.assets){
+         println '  -- ' + asset.downloadUrl // + ' (' + asset.id + ')'
+      }
+   }
+
+   static def printMaven(it){
+      println '  <dependency>' 
+      println '    <groupId>' + it.group + '</group>'
+      println '    <artifactId>' + it.name + '</artifactId>'
+      println '    <version>' + it.version + '</version>'
+      println '  </dependency>'
+   }
+
+   static def printNpm(it){
+      println '  "' + it.name + '" : "' + it.version + '"'
+   }
+
+   static def printDocker(it, dockerRepoConnector){
+      println dockerRepoConnector + '/' + it.name + ':' + it.version 
+   }
+
+}
+
+
+
+
+
